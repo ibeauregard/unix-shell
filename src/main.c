cd75dae3462c1bb7f65ebf61dc4bcdd5cb5eead4
Ian Beauregard <ian.beauregard@gmail.com>
2021-04-17 22:28:17 -0400

    Make test case more explicit

--- src/main.c
+++ src/main.c
@@ -14,9 +14,9 @@ int main(int argc, char* argv[], char* envp[])
     shell.execute(CommandLineClass.fromString("echo $PATH cyr"));
     shell.execute(CommandLineClass.fromString("echo $PICKUP"));
     shell.execute(CommandLineClass.fromString("echo eric $PICKUP"));
-    shell.execute(CommandLineClass.fromString("/roger -a"));
-    shell.execute(CommandLineClass.fromString("/bin/ls -a"));
+    shell.execute(CommandLineClass.fromString("/not_found -a"));
     shell.execute(CommandLineClass.fromString("./permission_denied"));
+    shell.execute(CommandLineClass.fromString("/bin/ls -a"));
     shell.delete();
     return EXIT_SUCCESS;
 }
