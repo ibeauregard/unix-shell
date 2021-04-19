31cc6f19e27b5c67856b2886dd70453a98177eed
Ian Beauregard <ian.beauregard@gmail.com>
2021-04-18 22:23:24 -0400

    Stop tracking environment's length (number of variables)

--- src/environment.c
+++ src/environment.c
@@ -10,7 +10,6 @@ const struct environment_class EnvironmentClass = {
 typedef struct variable_node VariableNode;
 struct internals {
     VariableNode* head;
-    size_t length;
 };
 
 static void insert(Environment* this, Variable* variable);
@@ -25,11 +24,9 @@ Environment* from_string_array(char* envp[])
     Environment* this = malloc(sizeof (Environment));
     this->_internals = malloc(sizeof (struct internals));
     this->_internals->head = NULL;
-    size_t i;
-    for (i = 0; envp[i]; i++) {
+    for (size_t i = 0; envp[i]; i++) {
         insert(this, VariableClass.fromString(envp[i]));
     }
-    this->_internals->length = i;
     this->getValueFromId = &get_value_from_id;
     this->setVariable = &set_variable;
     this->unsetVariable = &unset_variable;
@@ -113,10 +110,12 @@ void print(Environment* this)
 
 char** serialize(Environment* this)
 {
-    char** serialized = malloc((this->_internals->length + 1) * sizeof (char*));
+    size_t length;
     VariableNode* node = this->_internals->head;
+    for (length = 0; node; node = node->next, length++);
+    char** serialized = malloc((length + 1) * sizeof (char*));
     size_t i;
-    for (i = 0; node; ++i, node = node->next) {
+    for (i = 0, node = this->_internals->head; node; node = node->next, i++) {
         serialized[i] = node->variable->toString(node->variable);
     }
     serialized[i] = NULL;
