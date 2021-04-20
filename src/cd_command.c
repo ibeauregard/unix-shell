75b28fc79a0f5d1f6d99eac50e34ee78bd00bc1b
Ian Beauregard <ian.beauregard@gmail.com>
2021-04-20 18:31:37 -0400

    Implement part of the cd algorithm

--- src/cd_command.c
+++ src/cd_command.c
@@ -1,6 +1,10 @@
 #include "cd_command.h"
+#include "shell.h"
+#include "environment.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
 
 static Command* with_args(StringList* arguments);
 const struct cd_command CdCommand = {
@@ -21,7 +25,7 @@ Command* with_args(StringList* arguments)
 
 struct internals {
     bool pOption;
-    char* directory;
+    char* operand;
     bool error;
 };
 
@@ -29,7 +33,7 @@ void initialize_internals(Command* this)
 {
     this->_internals = malloc(sizeof (struct internals));
     this->_internals->pOption = false;
-    this->_internals->directory = NULL;
+    this->_internals->operand = NULL;
     this->_internals->error = false;
 }
 
@@ -43,7 +47,7 @@ void parse_arguments(Command* this, StringList* arguments)
         handle_flag_argument(this, arguments->next(arguments));
     }
     if (!this->_internals->error && !arguments->isEmpty(arguments)) {
-        this->_internals->directory = arguments->next(arguments);
+        this->_internals->operand = arguments->next(arguments);
     }
     if (!this->_internals->error && !arguments->isEmpty(arguments)) {
         this->_internals->error = true;
@@ -65,15 +69,130 @@ void handle_flag_argument(Command* this, char* argument)
     free(argument);
 }
 
+static void do_execute(Command* this);
 static void delete(Command* this);
 void execute(Command* this)
 {
+    if (!this->_internals->error) do_execute(this);
     delete(this);
+}
+
+struct state {
+    char* curpath;
+    char* home_value;
+    char* cdpath_value;
+    char* oldpwd_value;
+    bool print_new_directory_name;
+    bool interrupted;
+};
+
+static struct state initialize_state();
+static void set_operand(Command* this, struct state* state);
+static bool starts_with_slash_or_dot_or_dot_dot(char* string);
+static void browse_cdpath(char* operand, struct state* state);
+static void free_state(struct state* state);
+void do_execute(Command* this)
+{
+    struct state state = initialize_state();
+    set_operand(this, &state);
+    if (!state.interrupted
+        && !starts_with_slash_or_dot_or_dot_dot(this->_internals->operand)) {
+        browse_cdpath(this->_internals->operand, &state);
+    }
+    if (!state.interrupted && !state.curpath) state.curpath = strdup(this->_internals->operand);
+    if (!state.interrupted && !this->_internals->pOption)
+
+    free_state(&state);
+}
+
+struct state initialize_state()
+{
+    return (struct state){
+        .curpath = NULL,
+        .home_value = shell.environment->getValueFromId(shell.environment, "HOME"),
+        .cdpath_value = shell.environment->getValueFromId(shell.environment, "CDPATH"),
+        .oldpwd_value = shell.environment->getValueFromId(shell.environment, "OLDPWD"),
+        .print_new_directory_name = false,
+        .interrupted = false
+    };
+}
+
+void set_operand(Command* this, struct state* state)
+{
+    if (!this->_internals->operand) {
+        if (!strlen(state->home_value)) {
+            free_state(state);
+            state->interrupted = true;
+        } else {
+            this->_internals->operand = strdup(state->home_value);
+        }
+    }  else if (!strcmp(this->_internals->operand, "-") && strlen(state->oldpwd_value) > 0) {
+        free(this->_internals->operand);
+        this->_internals->operand = strdup(state->oldpwd_value);
+        state->print_new_directory_name = true;
+    } else if (!strcmp(this->_internals->operand, "~") && strlen(state->home_value) > 0) {
+        free(this->_internals->operand);
+        this->_internals->operand = strdup(state->home_value);
+    }
+}
+
+bool starts_with_slash_or_dot_or_dot_dot(char* string)
+{
+    return string[0] == '/'
+           || (strlen(string) == 1 && string[0] == '.')
+           || (strlen(string) >= 2 && string[0] == '.' && string[1] == '/')
+           || (strlen(string) == 2 && string[0] == '.' && string[1] == '.')
+           || (strlen(string) >= 3 && string[0] == '.' && string[1] == '.' && string[2] == '/');
+}
+
+static char* get_concatenation(char* pathname, char* operand);
+static bool names_a_directory(char* string);
+void browse_cdpath(char* operand, struct state* state)
+{
+    StringList* cdpathElements = StringListClass.split(state->cdpath_value, ':');
+    while (!state->curpath && !cdpathElements->isEmpty(cdpathElements)) {
+        char* pathname = cdpathElements->next(cdpathElements);
+        char* concatenation = get_concatenation(pathname, operand);
+        if (names_a_directory(concatenation)) state->curpath = concatenation;
+        else free(concatenation);
+        free(pathname);
+    }
+    if (!state->curpath) {
+        char* concatenation = get_concatenation(".", operand);
+        if (names_a_directory(concatenation)) state->curpath = concatenation;
+        else free(concatenation);
+    }
+    cdpathElements->delete(cdpathElements);
+}
+
+char* get_concatenation(char* pathname, char* operand)
+{
+    size_t pathname_length = strlen(pathname);
+    char* concatenation = malloc(pathname_length + (pathname[pathname_length - 1] != '/') + strlen(operand) + 1);
+    strcpy(concatenation, pathname);
+    if (pathname[pathname_length - 1] != '/') strcat(concatenation, "/");
+    strcat(concatenation, operand);
+    return concatenation;
+}
+
+bool names_a_directory(char* string)
+{
+    struct stat statbuf;
+    if (stat(string, &statbuf) == -1) return false;
+    return S_ISDIR(statbuf.st_mode);
+}
+
+void free_state(struct state* state)
+{
+    if (state->curpath) free(state->curpath);
+    free(state->home_value);
+    free(state->cdpath_value);
+    free(state->oldpwd_value);
 }
 
 void delete(Command* this)
 {
-    free(this->_internals->directory);
+    free(this->_internals->operand);
     free(this->_internals);
     free(this); this = NULL;
 }
